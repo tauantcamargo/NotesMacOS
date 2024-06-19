d92152eb2c411a82fee7c9c49bd7f10e53bc5670
Tauan Camargo <lukyhenson@gmail.com>
2024-06-19 01:09:00 -0300

    feat: add colors to add button

--- NotesMacOS/View/Home.swift
+++ NotesMacOS/View/Home.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct Home: View {
+    @State var showColors: Bool = false
+    
     var body: some View {
-        HStack {
+        HStack(spacing: 0) {
             // Side bar ...
             if isMacOS() {
-                SideBar()
+                Group {
+                    SideBar()
+                    
+                    Rectangle()
+                        .fill(Color.gray.opacity(0.15))
+                        .frame(width: 1)
+                }
             }
+            // Main Content
+            
         }
-        .frame(width: isMacOS() ? getRect().width / 1.7 : nil, height: isMacOS() ? getRect().height - 180 : nil)
+        .ignoresSafeArea()
+        .frame(width: isMacOS() ? getRect().width / 1.7 : nil, height: isMacOS() ? getRect().height - 180 : nil, alignment: .leading)
         .background(Color("BG").ignoresSafeArea())
         .preferredColorScheme(.light)
     }
@@ -29,13 +40,39 @@ struct Home: View {
             
             // Add Button
             AddButton()
+            
+            // Colors
+            VStack(spacing: 15){
+                let colors = [
+                    Color(.orange),
+                    Color(.red),
+                    Color(.purple),
+                    Color(.blue),
+                    Color(.green)
+                ]
+                
+                ForEach(colors,id: \.self){ color in
+                    Circle()
+                        .fill(color)
+                        .frame(width: 20, height: 20)
+                }
+            }
+            .padding(.top,20)
+            .frame(height: showColors ? nil : 0)
+            .opacity(showColors ? 1 : 0)
         }
+        .frame(maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .top)
+        .padding(.vertical)
+        .padding(.horizontal,22)
+        .padding(.top, 35)
     }
     
     @ViewBuilder
     func AddButton()->some View{
         Button {
-            
+            withAnimation{
+                showColors.toggle()
+            }
         } label: {
             Image(systemName: "plus")
                 .font(.title2)
@@ -44,6 +81,7 @@ struct Home: View {
                 .background(Color.black)
                 .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
         }
+        .padding(.top,30)
     }
 }
 
