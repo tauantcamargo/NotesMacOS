5f19b671bfbac037ca803c9483ed3ddcd0acbae8
Tauan Camargo <lukyhenson@gmail.com>
2024-06-19 00:33:57 -0300

    Initial Commit

---
+++ NotesMacOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NotesMacOS
+//
+//  Created by Tauan Tathiell Camargo on 19/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NotesMacOS/NotesMacOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesMacOSApp.swift
+//  NotesMacOS
+//
+//  Created by Tauan Tathiell Camargo on 19/06/24.
+//
+
+import SwiftUI
+
+@main
+struct NotesMacOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
